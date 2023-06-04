2bdc0a5e7e9cfd405a343eb454f27864b6ff8f61
LironKruchininGGo <131599143+LironKruchininGGo@users.noreply.github.com>
2023-06-04 17:00:17 +0300

    Fixed structure of BE removing user.controller

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from '../../entity/user.entity';
@@ -7,13 +6,11 @@ import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
-    // AuthModule,
     MongooseModule.forFeature([
       { name: 'User', schema: UserSchema }
     ])
   ],
   providers: [UserService],
-  controllers: [UserController],
   exports: [UserService]
 })
 export class UserModule { }
