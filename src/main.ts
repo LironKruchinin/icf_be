675ba5725e40e245561a7890d3a63fd7253c8e2e
LironKruchininGGo <lironkruch@gmail.com>
2023-08-17 18:15:11 +0300

    removed https because of a bug that caused runtime error

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import * as cookieParser from 'cookie-parser';
-import { readFileSync } from 'fs';
+import * as fs from 'fs';
 import * as https from 'https';
 import * as path from 'path';
 
 async function bootstrap() {
   dotenv.config();
-  const keyPath = path.join(__dirname, '..', './keys/localhost-key.pem');
-  const certPath = path.join(__dirname, '..', './keys/localhost.pem');
+
+  const keyPath = path.join(__dirname, '..', 'keys', 'localhost-key.pem');
+  const certPath = path.join(__dirname, '..', 'keys', 'localhost.pem');
 
   const httpsOptions = {
-    key: readFileSync(keyPath),
-    cert: readFileSync(certPath),
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath),
   };
 
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: 'http://localhost:3001',
@@ -24,7 +25,7 @@ async function bootstrap() {
   });
 
   app.use(cookieParser());
-  await app.listen(3000)
+  await app.listen(3000);
 }
 
 bootstrap();
