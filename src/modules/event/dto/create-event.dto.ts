6f90582430d259dfc5922abe38c3d4269de458a8
LironKruchininGGo <lironkruch@gmail.com>
2023-08-29 23:19:21 +0300

    used reducedUser class

--- src/modules/event/dto/create-event.dto.ts
+++ src/modules/event/dto/create-event.dto.ts
@@ -1,7 +1,10 @@
+import { ReducedUser } from "src/entity/user.entity";
+
 export class CreateEventDto {
     eventName: string;
     eventDescription: string;
     eventDate: number;
     eventCloseDate: number;
     createdAt: number;
+    users?: ReducedUser[];
 }
