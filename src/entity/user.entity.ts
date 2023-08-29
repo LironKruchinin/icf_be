d99287676df7b27f0db10f52d1d4c192e0ea5a1f
LironKruchininGGo <lironkruch@gmail.com>
2023-08-29 23:16:40 +0300

    changed the structure for user entity

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,4 +1,37 @@
 import { Document, Schema } from 'mongoose'
+import { GameRole } from 'src/modules/game-role/dto/create-game-role.dto';
+
+const GameRoleSchema = new Schema({
+    _id: { type: String },
+    gameRoleName: { type: String }
+})
+
+const userGroups = new Schema({
+    _id: { type: String },
+    groupName: { type: String }
+})
+
+
+export interface ReducedUser {
+    _id: string;
+    email: string;
+    first_name: string;
+    user_name: string;
+    gameRole: GameRole[] | null;
+    userGroups: Group[] | null;
+}
+
+class Group {
+    _id: string;
+    groupName: string;
+}
+
+export type DeleteResult = {
+    n?: number;
+    ok?: number;
+    deletedCount?: number;
+};
+
 
 export const UserSchema = new Schema({
     email: { type: String, required: true },
@@ -15,6 +48,7 @@ export const UserSchema = new Schema({
 })
 
 export interface User extends Document {
+    _id: string;
     email: string;
     password: string;
     first_name: string;
@@ -23,7 +57,26 @@ export interface User extends Document {
     created_at: number;
     updated_at: number;
     salt: string;
-    roles: [string];
-    gameRole: [string] | null;
+    roles: string[];
+    gameRole: GameRole[] | null;
     user_color: string;
+    userGroups: Group[] | null;
+}
+
+export const ReducedUserSchema = new Schema({
+    email: { type: String, required: true },
+    first_name: { type: String, required: true },
+    user_name: { type: String, required: true },
+    // created_at: { type: Number, required: true },
+    // updated_at: { type: Number },
+    // roles: { type: [String], default: ['basic'] },
+    userGroups: { type: [userGroups] },
+    gameRole: { type: [GameRoleSchema] },
+})
+
+
+export class BasicUserData {
+    _id: string;
+    first_name: string;
+    user_name: string;
 }
