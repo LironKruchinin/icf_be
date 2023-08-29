ddbd96fa8a97040651bb7548b0aa7eaa563f00f6
LironKruchininGGo <lironkruch@gmail.com>
2023-08-29 23:15:39 +0300

    added new dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { EventModule } from './modules/event/event.module';
+import { GroupsModule } from './modules/groups/groups.module';
+import { GameRoleModule } from './modules/game-role/game-role.module';
+import { RoleModule } from './modules/role/role.module';
 import * as cookieParser from 'cookie-parser';
 @Module({
   imports: [
@@ -11,7 +14,10 @@ import * as cookieParser from 'cookie-parser';
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     AuthModule,
-    EventModule
+    EventModule,
+    GroupsModule,
+    GameRoleModule,
+    RoleModule
   ],
   controllers: [],
   providers: [],
