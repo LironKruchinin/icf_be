9f6e70cd77ce37b074fa6d19417720cbcd041fa3
LironKruchininGGo <lironkruch@gmail.com>
2023-08-29 23:19:06 +0300

    added user db and service, fixed create, added and remove user for event

--- src/modules/event/event.service.ts
+++ src/modules/event/event.service.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@nestjs/common';
-import { CreateEventDto } from './dto/create-event.dto';
-import { UpdateEventDto } from './dto/update-event.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { DeleteResult, Event } from 'src/entity/event.entity';
+import { BasicUserData, User } from 'src/entity/user.entity';
+import { UserService } from '../user/user.service';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
 
 @Injectable()
 export class EventService {
-  constructor(@InjectModel('Event') private eventModel: Model<Event>) { }
+  constructor(
+    private readonly userService: UserService,
+    @InjectModel('User') private userModel: Model<User>,
+    @InjectModel('Event') private eventModel: Model<Event>,
+  ) { }
 
   async create(createEventDto: CreateEventDto) {
+    const users = (await this.userService.getAllUsers())
+    const members = users.filter(user =>
+      user.roles.includes('member') ||
+      user.roles.includes('owner') ||
+      user.roles.includes('admin')
+    ).map(user => ({
+      _id: user._id,
+      email: user.email,
+      first_name: user.first_name,
+      user_name: user.user_name,
+      gameRole: user.gameRole,
+      userGroups: user.userGroups,
+    }))
+    createEventDto.users = members
     return this.eventModel.create(createEventDto)
   }
 
@@ -27,6 +47,22 @@ export class EventService {
 
   async remove(id: string): Promise<DeleteResult> {
     return this.eventModel.deleteOne({ _id: id }).exec();
+  }
+
+  addUserToRole(id: string, basicUserData: BasicUserData) {
+    return this.eventModel.findByIdAndUpdate(id,
+      { $push: { users: basicUserData } },
+      { new: true, upsert: true })
+      .lean()
+      .exec()
+  }
+
+  removeUserToRole(id: string, userId: string) {
+    return this.eventModel.findByIdAndUpdate(id,
+      { $pull: { users: userId } },
+      { new: true })
+      .lean()
+      .exec()
   }
 
 }
