1934d79007e485ae8ea2ae2b20787a5ca1bbbe2f
LironKruchininGGo <lironkruch@gmail.com>
2023-08-29 23:18:33 +0300

    added user schema and service for event

--- src/modules/event/event.module.ts
+++ src/modules/event/event.module.ts
@@ -3,14 +3,17 @@ import { EventService } from './event.service';
 import { EventController } from './event.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EventSchema } from 'src/entity/event.entity';
+import { UserSchema } from 'src/entity/user.entity';
+import { UserService } from '../user/user.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
+      { name: 'User', schema: UserSchema },
       { name: 'Event', schema: EventSchema }
     ])
   ],
   controllers: [EventController],
-  providers: [EventService]
+  providers: [EventService, UserService]
 })
 export class EventModule { }
